209ac7b610c94e2fb35ca0b5021cab3c38fe4303
LakshmeeshSR <118297152+LakshmeeshSR@users.noreply.github.com>
2023-02-22 16:39:41 +0530

    Update Main_Home.py

--- Main_Home.py
+++ Main_Home.py
@@ -165,7 +165,7 @@ def show_main_page():
 #         image_2 = Image.open(r"")
 #         st.image(image_2,width=250)
         
-        response = requests.get("https://raw.githubusercontent.com/LakshmeeshSR/DigitalCV/main/SmartAC-removebg.png?token=GHSAT0AAAAAAB7DTZYOJRTUG2LG7MEK6EKCY7V2QOA")
+        response = requests.get("https://github.com/LakshmeeshSR/DigitalCV/blob/main/SmartAC-removebg.png")
         img = Image.open(BytesIO(response.content))
         st.image(img, width=230)
     
